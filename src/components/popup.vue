621538b8b23337e9afd7005b0471e052e72f7924
yonghee <yonghee@yongheeui-iMac.local>
2021-01-05 17:31:13 +0900

    2021년 1월  5일 화요일 17시 31분 13초 KST

--- src/components/popup.vue
+++ src/components/popup.vue
@@ -1,45 +1,49 @@
 <template>
-  <v-dialog
-      v-model="dialog"
-      width="unset"
-    >
-      <div class="popup">
-        <a><img src="/img/main_popup.png"></a>
-        <v-btn icon class="closeBtn" @click="dialog = false">
-          <v-icon>mdi-close</v-icon>
-        </v-btn>
-      </div>
+  <v-dialog v-model="dialog" width="unset">
+    <div class="popup">
+      <a @click="openPdf"><img src="/img/main_popup.png" /></a>
+      <v-btn icon class="closeBtn" @click="dialog = false">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </div>
   </v-dialog>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        dialog: true,
-      }
+export default {
+  data() {
+    return {
+      dialog: true,
+    };
+  },
+  methods: {
+    openPdf() {
+      const filePath = `http://www.payot-inc.com/pdf/kiosk_2020.pdf`;
+      window.open(filePath, "_blank");
     },
-  }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
-.popup{
+.popup {
   position: relative;
 
-  a{
+  a {
     cursor: pointer;
   }
 
-  .closeBtn{
-    position:absolute;
-    width:40px;
-    height:40px;
-    top:20px;
-    right:20px;
-    background:#292929;
+  .closeBtn {
+    position: absolute;
+    width: 40px;
+    height: 40px;
+    top: 20px;
+    right: 20px;
+    background: #292929;
 
-    .v-icon{color:#fff;}
-  }  
+    .v-icon {
+      color: #fff;
+    }
+  }
 }
 </style>
